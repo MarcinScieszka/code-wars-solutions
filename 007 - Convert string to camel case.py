696ac6546729b62e0370dfd60bf371dbda473b8a
Marcin Scieszka <scieszka.marcin@pm.me>
2021-07-03 17:51:17 +0200

    Update 007 - Convert string to camel case.py
    Added solution

--- 007 - Convert string to camel case.py
+++ 007 - Convert string to camel case.py
@@ -10,4 +10,21 @@ Examples
 """
 
 def to_camel_case(text):
-    pass
+    if len(text) == 0:
+        return text
+
+    first_letter = text[0]
+
+    # replace every dash with underscore and devide text into words
+    text = text.replace('-', '_').split('_')
+
+    converted_text = ''
+    for word in text:
+        # each word starts with capital letter 
+        converted_text += word.capitalize()
+
+    if first_letter != converted_text[0]:
+        # first letter from the original text has to be preserved
+        converted_text = first_letter + converted_text[1:]
+
+    return converted_text
