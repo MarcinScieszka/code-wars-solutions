0aa1054bd151983ea7a5265faf2a87cc9a394015
Marcin Scieszka <marcin.scieszka@student.pk.edu.pl>
2021-07-03 14:35:54 +0200

    Added solution

--- 004 - Replace With Alphabet Position.py
+++ 004 - Replace With Alphabet Position.py
@@ -12,5 +12,17 @@ alphabet_position("The sunset sets at twelve o' clock.")
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 """
 
+# provide 'abcdefghijklmnopqrstuvwxyz'
+from string import ascii_lowercase 
+
 def alphabet_position(text):
-    pass
+    # genereate dict where {'a': 1, 'b': 2, ..., 'z': 26}
+    repacements = {letter.lower():i+1 for i, letter in enumerate(ascii_lowercase)}
+
+    output_string = ''
+    for character in text:
+        if character.lower() in repacements:
+            # lower() used to account for upper letters
+            output_string += ' ' + str(repacements[character.lower()]) 
+    
+    return output_string.strip() # strip() used to remove unwanted space
