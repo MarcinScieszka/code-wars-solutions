7156de08698bdfd61d58155059513f4e9fea3bf8
Marcin Scieszka <scieszka.marcin@pm.me>
2021-07-06 12:47:44 +0200

    Update 011 - Sort the odd.py
    added initial solution

--- 011 - Sort the odd.py
+++ 011 - Sort the odd.py
@@ -9,5 +9,30 @@ Examples
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 """
 
+
 def sort_array(source_array):
-    pass
+    """function sorts odd numbers from source_array in ascending order while leaving even numbers as they were"""
+    
+    odds = []
+
+    # res retains even values as they were in source_array and None insdead of odd values
+    # odd values added to odds as a tuple: (index, value in a given index)
+    res = [source_array[idx] if (source_array[idx] % 2 == 0) else odds.append((idx,source_array[idx])) for idx in range(len(source_array))]
+   
+    # sort odds by value (by second element in a tuple)
+    odds.sort(key=lambda elem: elem[1])
+
+    # replace tuple with values only
+    sorted_odd_values = [odd_val[1] for odd_val in odds]
+
+    # add ordered odd values in a place of None values to the result array 
+    j = 0
+    for i in range(len(source_array)):
+        if res[i] == None:
+            res[i] = sorted_odd_values[j]
+            j += 1
+
+    return res
+
+
+print(sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
