042905b352ae3c69a6abbb6131e87f6dd51e2aa4
Marcin Scieszka <scieszka.marcin@pm.me>
2021-07-07 18:10:15 +0200

    Update 013 - Moving Zeros To The End.py
    added solution

--- 013 - Moving Zeros To The End.py
+++ 013 - Moving Zeros To The End.py
@@ -5,4 +5,20 @@ move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 """
 
 def move_zeros(array):
+    # function takes an array and moves all of the zeros to the end, preserving the order of the other elements
+
+    shift = 0 # stores amount of encountered zeroes in the array
+    for idx, val in enumerate(array):
+        if val == 0:
+            # zero is encountered
+            shift += 1
+        if shift and val != 0:
+            # there is at least one zero in the array (if false there is no need to change order in the array) and the current value is nonzero (so that shift amount is correct)
+            # place nonzero element shifted to the left by shift amount
+            array[idx - shift] = val
+    
+    for i in range(shift):
+        # place shift amount of zeroes at the end of the array
+        array[-(i+1)] = 0
+
     return array
