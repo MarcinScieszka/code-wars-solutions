6187821fe103b31c92f3978abb1a1c68fe38e94c
Marcin Scieszka <scieszka.marcin@pm.me>
2021-07-09 22:16:38 +0200

    Update 014 - Sum by Factors.py
    added another solution where instead of checking if a value is prime, we check if the value exists in a set containing prime numbers up to the max value in the lst, which is 1.55 faster than first implementation
    NOTE: usage of max(lst) is as fast as (numpy.array(lst)).max() (0.045% difference). It seems that speed advantage gained by using numpy is lost while converting list to np.array.

--- 014 - Sum by Factors.py
+++ 014 - Sum by Factors.py
@@ -20,13 +20,14 @@ Example: I = [15, 30, -45] 5 divides 15, 30 and (-45) so 5 appears in the result
     In Fortran - as in any other language - the returned string is not permitted to contain any redundant trailing whitespace: you can use dynamically allocated character strings.
 """
 
-
 import sympy
-
+import numpy
+import time
+import random
 
 def sum_for_list(lst):
     prime_factors = {} # key=prime_factor, val=values which contain given prime factor
-
+    
     # determining prime factors for each value in the lst
     for val in lst:
         for i in range(val//2):
@@ -38,5 +39,65 @@ def sum_for_list(lst):
     
     return result
 
+def psieve():
+    # https://stackoverflow.com/a/19391111/15327830
+    import itertools
+    yield from (2, 3, 5, 7)
+    D = {}
+    ps = psieve()
+    next(ps)
+    p = next(ps)
+    assert p == 3
+    psq = p*p
+    for i in itertools.count(9, 2):
+        if i in D:      # composite
+            step = D.pop(i)
+        elif i < psq:   # prime
+            yield i
+            continue
+        else:           # composite, = p*p
+            assert i == psq
+            step = 2*p
+            p = next(ps)
+            psq = p*p
+        i += step
+        while i in D:
+            i += step
+        D[i] = step
 
-print(sum_for_list([15, 21, 24, 30, 45]))
+def sum_for_list_2(lst):
+    prime_factors = {} # key=prime_factor, val=values which contain given prime factor
+
+    # making set of prime numbers up to max element in the lst
+    primes_gen = psieve()
+    primes = set([next(primes_gen) for i in range(max(lst))])
+
+    # determining prime factors for each value in the lst
+    for val in lst:
+        for i in range(val//2):
+            if i in primes and val % i == 0:
+                prime_factors.setdefault(i,[]).append(val)
+
+    # array of arrays where values are: prime factor and sum of the numbers for which given value is a prime factor
+    result = [[key, sum(prime_factors[key])] for key in sorted(prime_factors)]
+    
+    return result
+
+
+a = [random.randint(10, 10e5) for i in range(random.randint(5, 15))]
+res1 = []
+res2 = []
+
+for i in range(10):
+    start = time.time()
+    sum_for_list(a)
+    end = time.time()
+    res1.append(end - start)
+
+    start = time.time()
+    sum_for_list_2(a)
+    end = time.time()
+    res2.append(end - start)
+
+print(numpy.mean(res1))
+print(numpy.mean(res2))
