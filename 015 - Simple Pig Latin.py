4d34f4f85c030d58737445ef65a70979c9a7d7e1
Marcin Scieszka <scieszka.marcin@pm.me>
2021-07-13 21:27:36 +0200

    Update 015 - Simple Pig Latin.py
    added solution

--- 015 - Simple Pig Latin.py
+++ 015 - Simple Pig Latin.py
@@ -7,5 +7,25 @@ pig_it('Pig latin is cool') # igPay atinlay siay oolcay
 pig_it('Hello world !')     # elloHay orldway !
 """
 
-def pig_it(text):
-    pass
+
+def pig_it(text: str) -> str:
+    """
+    fuction takes text, returns modified text, where the first letter of each word is moved to the end of it, with added "ay"
+    punctuation marks are untouched
+    """
+
+    result = ''
+    for word in text.split():
+        # split text by spaces
+        if word.isalpha():
+            # all the characters are alphabet letters (a-z)
+            result += ' ' + word[1:] + word[0] + 'ay'
+        else:
+            # other characters are untouched
+            result += ' ' + word
+
+    return result.lstrip()
+
+
+print(pig_it('This is my string') == 'hisTay siay ymay tringsay')
+print(pig_it('Quis custodiet ipsos custodes ?') == 'uisQay ustodietcay psosiay ustodescay ?')
