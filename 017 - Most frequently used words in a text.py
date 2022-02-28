95e9fa68f8a85e968358b1e25d3598bb2ccc2ddf
MarcinScieszka <79609951+MarcinScieszka@users.noreply.github.com>
2022-02-28 11:37:39 +0100

    implement solution

--- 017 - Most frequently used words in a text.py
+++ 017 - Most frequently used words in a text.py
@@ -27,10 +27,43 @@ Examples:
     # => ["won't", "wont"]
 """
 
+import re
+import string
 
-def top_3_words(text):         
-    return text
+def top_3_words(text):
+    top_words = {}
+
+    # if char is not a leter or apostrophe replace it with space
+    allowed_chars = string.ascii_lowercase + string.ascii_uppercase + "'"
+    for c in text:
+        if c not in allowed_chars:
+            text = text.replace(c, " ")
+ 
+    # remove duplicate spaces
+    text = re.sub(" +", " ", text)
+
+    # remove spaces from the begining and end of the text
+    text = text.strip()
+    
+    words = text.split(' ')
+    
+    # fill a dict with words as keys and their occurrences as vals
+    for word in words:
+        # words in the result should be lowercased
+        word = word.lower()
+
+        if word not in top_words:
+            top_words[word] = 1
+        else:
+            top_words[word] += 1
+
+    # sort dict in descending order and reduce size to top three occuring words  
+    top_three_dict =  dict(reversed(sorted(top_words.items(), key=lambda item: item[1])[-3:]))
+
+    return list(top_three_dict.keys())
 
 
-text = "In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income."
-print(top_3_words(text))
+text1 = "In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income."
+text2 = "e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"
+text3 = "  //wont won't won't"
+print(top_3_words(text1))
